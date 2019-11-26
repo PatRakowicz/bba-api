8604998edf8d293b31f63c9576a4eb5ef9fa81e7
Patrick Rakowicz <lividsubset3@gmail.com>
2019-11-26 13:28:53 -0700

    Spring Boot int

---
+++ src/main/java/com/bba/bbaapi/BbaApiApplication.java
@@ -0,0 +1,13 @@
+package com.bba.bbaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbaApiApplication.class, args);
+	}
+
+}
